55925cf56024652145a138bee479747647fb164e
hari-testpress <hari@testpress.in>
2023-05-03 13:14:12 +0530

    Refactor unwanted code

--- iPlayer2/Views/VideoPlayerSlider.swift
+++ iPlayer2/Views/VideoPlayerSlider.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class VideoPlayerSlider: UISlider {
     var currentBuffer: Float = 0.5
-    var bufferedLayer: CALayer!
     
     override init(frame: CGRect)
     {
@@ -27,23 +26,6 @@ class VideoPlayerSlider: UISlider {
     fileprivate func initialize(){
         changeSliderThumbSize()
         self.maximumTrackTintColor = UIColor.lightGray.withAlphaComponent(0.4)
-        bufferedLayer = CALayer()
-        bufferedLayer.backgroundColor = UIColor.gray.cgColor
-        if let subviews = subviews as? [UIView] {
-            for subview in subviews {
-                print(subview)
-                if let progressBar = subview as? UIProgressView {
-                    print("called")
-                    progressBar.layer.addSublayer(bufferedLayer)
-                }
-            }
-        }
-        updateBufferedLayer()
-    }
-    
-    fileprivate func updateBufferedLayer() {
-        let bufferedWidth = self.bounds.width * CGFloat(currentBuffer)
-        bufferedLayer.frame = CGRect(x: 0, y: 0, width: bufferedWidth, height: 2.5)
     }
     
     fileprivate func changeSliderThumbSize() {
@@ -56,28 +38,4 @@ class VideoPlayerSlider: UISlider {
        newBounds.size.height = 2.5
        return newBounds
     }
-    
-    
-    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-        let beganTracking = super.beginTracking(touch, with: event)
-        showTooltip()
-        return beganTracking
-    }
-    
-    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-        let continuedTracking = super.continueTracking(touch, with: event)
-        updateTooltip()
-        return continuedTracking
-    }
-    
-    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
-        super.endTracking(touch, with: event)
-        hideTooltip()
-    }
-    
-    func showTooltip(){}
-    
-    func updateTooltip(){}
-    
-    func hideTooltip(){}
 }
