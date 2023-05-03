b6c864c1426406631f40cc18b7e33702592f3e7d
hari-testpress <hari@testpress.in>
2023-05-03 13:02:05 +0530

    Change non-DRM video'

--- iPlayer2/Constants.swift
+++ iPlayer2/Constants.swift
@@ -7,7 +7,7 @@
 struct Constants{
     struct Videos {
         static let DRM_PROTECTED_VIDEO = "https://d384padtbeqfgy.cloudfront.net/transcoded/580f8a44-f46b-4166-8717-d2610c084ee0/video.mpd"
-        static let NON_DRM_PROTECTED_VIDEO = "https://d384padtbeqfgy.cloudfront.net/transcoded/99F6XgQqxEr/video.m3u8"
+        static let NON_DRM_PROTECTED_VIDEO = "https://d384padtbeqfgy.cloudfront.net/transcoded/AeDsCzqB5Td/video.m3u8"
     }
 }
 
