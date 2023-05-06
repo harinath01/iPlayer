0df22669d383df2b05f60f53bae42d276abbcb43
hari-testpress <hari@testpress.in>
2023-05-06 15:47:40 +0530

    Add support to change video resolution

---
+++ iPlayer2/Utils/VideoQuality.swift
@@ -0,0 +1,13 @@
+//
+//  VideoQuality.swift
+//  iPlayer2
+//
+//  Created by Testpress on 06/05/23.
+//
+
+import Foundation
+
+struct VideoQuality {
+    var resolution: String
+    var bitrate: Int
+}
--- iPlayer2/Views/VideoPlayerView.swift
+++ iPlayer2/Views/VideoPlayerView.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UIKit
 import AVKit
+import M3U8Kit
 
 class VideoPlayerView: UIView, PlayerControlDelegate{    
     var player: AVPlayer!
@@ -13,6 +14,7 @@ class VideoPlayerView: UIView, PlayerControlDelegate{
     var isSeeking: Bool = false
     var frameInParentView: CGRect!
     var parentView: UIView?
+    var availableResolutions:[VideoQuality] = [VideoQuality(resolution:"Auto", bitrate: 0)]
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -33,6 +35,7 @@ class VideoPlayerView: UIView, PlayerControlDelegate{
         playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = self.bounds
         self.layer.addSublayer(playerLayer)
+        self.parseAvailableResolutionsFromManifest()
     }
     
     private func setupPlayerControls(){
@@ -61,6 +64,26 @@ class VideoPlayerView: UIView, PlayerControlDelegate{
         player?.currentItem?.addObserver(self, forKeyPath: "playbackBufferEmpty", options: .new, context: nil)
         player?.currentItem?.addObserver(self, forKeyPath: "playbackBufferFull", options: .new, context: nil)
         player.addObserver(self, forKeyPath: "timeControlStatus", options: .new, context: nil)
+    }
+    
+    func parseAvailableResolutionsFromManifest(){
+        do {
+            availableResolutions.removeAll()
+            availableResolutions = [VideoQuality(resolution:"Auto", bitrate: 0)]
+            let playlistModel = try M3U8PlaylistModel(url: url)
+            let masterPlaylist = playlistModel.masterPlaylist
+            guard let streamList = masterPlaylist?.xStreamList else {
+                return
+            }
+            streamList.sortByBandwidth(inOrder: .orderedAscending)
+            
+            for i in 0 ..< streamList.count {
+                if let extXStreamInf = streamList.xStreamInf(at: i){
+                    let resolution = "\(Int(extXStreamInf.resolution.height))p"
+                    availableResolutions.append(VideoQuality(resolution: resolution, bitrate: extXStreamInf.bandwidth))
+                }
+            }
+        } catch {}
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
@@ -196,16 +219,39 @@ class VideoPlayerView: UIView, PlayerControlDelegate{
         player.rate = speed.value
     }
     
+    func changeBitrate(_ bitrate: Int) {
+        player?.currentItem?.preferredPeakBitRate = Double(bitrate)
+    }
+    
     func showOptionsMenu(){
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Video Quality", style: .default, handler: { action in
-            print("Video Quality Speed")
+            self.showQualityMenu()
         }))
         alert.addAction(UIAlertAction(title: "Playback Speed", style: .default, handler: { action in
             self.showPlaybackSpeedMenu()
         }))
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        window?.rootViewController?.presentedViewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    func showQualityMenu(){
+        print(availableResolutions)
+        let alert = UIAlertController(title: "Playback Speed", message: nil, preferredStyle: .actionSheet)
+        for resolutionInfo in self.availableResolutions {
+            let action = UIAlertAction(title: resolutionInfo.resolution, style: .default, handler: { (_) in
+                self.changeBitrate(resolutionInfo.bitrate)
+            })
+            
+            if (Double(resolutionInfo.bitrate) == player?.currentItem?.preferredPeakBitRate) {
+                action.setValue(UIImage(named: "checkmark"), forKey: "image")
+            }
+            alert.addAction(action)
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
         window?.rootViewController?.presentedViewController?.present(alert, animated: true, completion: nil)
